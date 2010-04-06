dd840d5382478ebc66897929abd4c48d09b3e539
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2010-04-06 17:56:55 +0300

    Fix database migrations.

--- db/migrate/20091103222436_create_projects.rb
+++ db/migrate/20091103222436_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def self.up
     create_table :projects do |t|
       t.string :name
-      t.string :description
+      t.string :description, :limit => 1000
       t.datetime :start_date
       t.datetime :end_date
       t.timestamps
